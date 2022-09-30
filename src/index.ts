d8fc147a00a8cf907e007157f94345dce239036d
Cody Hatfield <cody.hatfield@me.com>
2022-09-29 18:04:38 -0700

    Fix SignerWithAddress

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import "@nomiclabs/hardhat-ethers";
 import { extendEnvironment } from "hardhat/config"
 import { SafeProviderAdapter } from "./adapter"
+import { Signer } from "ethers";
 
 export const setupSafeDeployer = (signerAddress: string, safeAddress: string, serviceUrl?: string) => {
     extendEnvironment((env) => {
         env.network.provider = new SafeProviderAdapter(
           env.network.provider,
-          env.ethers.getSigner(signerAddress),
+          env.ethers.getSigner(signerAddress).then(s => s as Signer),
           safeAddress,
           serviceUrl
         )
